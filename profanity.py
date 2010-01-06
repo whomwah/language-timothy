95cb7197e9a913a6d2239f9bb5a47d45edb3617e
Duncan Robertson <duncan@whomwah.com>
2010-01-06 09:42:46 +0000

    update swearwords and tweaked. Still not perfect

--- profanity.py
+++ profanity.py
@@ -12,17 +12,31 @@ def readProfanities():
 profanities = readProfanities()
 
 def replaceProfanity( text ):
-  results = [] 
-  for w in re.finditer(r"[\w']+", text):
-    if w.group(0).lower() in profanities:
-      results.append(w)
+  text = text.lower()
+  tmp = {} 
+  for w in profanities: 
+    w = w.split(':')
+    for m in re.finditer("[^\w+]?("+re.escape(w[0])+")[^\w+]?", text):
+      key_str = m.start()
+      if len(w) > 1:
+        for wl in w[1].split(','):
+          if wl not in text:
+            tmp[key_str] = m
+      else:
+        tmp[key_str] = m
   def compare(a, b):
     return cmp(int(a.end()), int(b.end()))
+  results = tmp.values() 
   results.sort(compare)
   results.reverse()
   return results
 
 if __name__ == '__main__':
-  w = "So you're an utter twat, and he's a complete dick shit, yes dick."
-  for r in replaceProfanity(w): 
-    print r.group(0), r.end()
+  w = """
+  Shit So you're an utter pussy, you turd, not saturday. <unt, [ock. So
+  dick what the fuck are you gonna fucking do. You dickheads suck cock
+  and I think you're a twat.
+  """
+  print w.strip()
+  for m in replaceProfanity(w): 
+    print m.group(1)[0] + '*'*(len(m.group(1))-2) + m.group(1)[len(m.group(1))-1] 
